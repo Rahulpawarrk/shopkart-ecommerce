0d004a8127c0de87e7dc391d3496097d3f6c94c7
Rahulp <Rahulp@STPL.com>
2026-08-18 19:36:44 +0530

    Switch SMTP to Port 465 SSL for cloud container firewall compatibility

--- src/main/java/com/example/ecommerce/util/EmailService.java
+++ src/main/java/com/example/ecommerce/util/EmailService.java
@@ -131,12 +131,16 @@ public class EmailService {
         }
     }
 
+    private static final int SMTP_PORT = 465;
+
     private Session buildMailSession() {
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", SMTP_HOST);
-        props.put("mail.smtp.port", String.valueOf(SMTP_PORT));
+        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.ssl.enable", "true");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.ssl.trust", SMTP_HOST);
         props.put("mail.smtp.connectiontimeout", "10000");
         props.put("mail.smtp.timeout", "10000");
